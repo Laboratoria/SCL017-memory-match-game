bcf7fe6421a0a1b0415af1de9ee4ff1d66913a12
EstefanyVergara <vteffa@gmail.com>
2021-06-24 09:18:36 -0400

    código javascript semi funcional

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-
 import pokemon from "../data/pokemon/pokemon.js";
 
 const App = () => {
@@ -17,55 +16,39 @@ const App = () => {
   level1.className = ("nivel1");
   level1.id = ("nivel1");
 
+  // funcionamiento del juego
+
   const memoryGame = document.createElement("div"); 
-  
-  
-  // shuffle(lista);
-  // let cartasEscogidas = [];
 
-  // function flipCard(target) {
-  //   cartasEscogidas.push(target);
-  //   if(cartasEscogidas.length === 2) {
-  //     setTimeout(match, 1000);
-  //   }
-  //   console.log(target);
-  //   console.log(cartasEscogidas)
-  // }
-  
-  // function match () {
-  //   if(cartasEscogidas[0] === cartasEscogidas[1]) {
-  //     alert('hiciste match');
-  //   } else {
-  //     alert('te equivocaste');
-  //     cartasEscogidas.length = 0;
-  //   }
-  // }
 
-  // for (let i = 0; i < lista.length; i++) {
-  
-  //   let card = document.createElement("div");
-  //   card.className = "card";
-  //   let imageFront = document.createElement("img");
-  //   imageFront.id = i+"";
-  //   imageFront.src = "ImageFront1.png";
-  //   imageFront.classname = "cardFrontImage";
-  //   card.appendChild(imageFront);
+  let listaPokemon = pokemon.items; // esto lo del for ya recorrido
+  listaPokemon = shuffle(listaPokemon); // toma la lista y la barajea 
+  listaPokemon = recortarLista(listaPokemon, 3);// recorta esa lista a solo 3 item y los hace doble 
+  listaPokemon = shuffle(listaPokemon); // vuelve a barajear lo anterior.
+  let cartasEscogidas = []; // esta array contenerá las cartas seleccionadas por el usuario 
 
-  //   let imagenBack = document.createElement('img');
-  //   imagenBack.id = i;
-  //   let cardId = imagenBack.id;
-  //   imagenBack.className = "card-back-img";
+// información declara en pokemones, se muestran en el contenedor
+  for (let index = 0; index < listaPokemon.length; index++) {
+    const element = listaPokemon[index]; // representa a cada item actual en la lista de pokemon 
+     const tarjeta = document.createElement ("div"); // aqui se crea un div x para que contenga al pokemon 
+     tarjeta.className=("card") // estilo para css
+     const estaImagen = document.createElement("img"); // img es especial para imagenes  
+    estaImagen.id = index+element.id; 
+     estaImagen.src= element.image
+
+    estaImagen.addEventListener("click", (event)=> {
+        tarjeta.classList.toggle('is-flipped');   
+        console.log (estaImagen)
+      }); 
+
+      
+    // cartasEscogidas.push(element)  //se agrega las cartas escogidas al array --- corregir cartas que son o no pares
+    memoryGame.appendChild(tarjeta); 
+    tarjeta.appendChild(estaImagen)
     
-  //   imagenBack.setAttribute("src", lista[cardId].image);
-  //   card.appendChild(imagenBack);
-
-  //   card.addEventListener("click", (event)=> {
-  //     card.classList.toggle('is-flipped');
-  //     flipCard(event.target);
-  //   });        
-
+  }
   
-
+    
 // Aquí anexamos todos los contenedores padres a sus respectivos contenedores hijos
   gameBoard.appendChild(gameContainer);
   gameContainer.appendChild(level1);
@@ -76,86 +59,30 @@ const App = () => {
   return gameBoard;
 };
 
+function recortarLista(lista) {
+  const listaRecortada = [];
+  for (let index = 0; index < 3; index++) {
+    const element = lista[index];
+    listaRecortada.push(element, element); //aquí se duplica un elemento de la lista, hasta llegar al index 3 (los tres primeros elemento
+  }
+  return listaRecortada;
+}
+const shuffle = (arr) =>{
+    for(var i =arr.length-1 ; i>0 ;i--){
+        var j = Math.floor( Math.random() * (i + 1) );
+        [arr[i],arr[j]]=[arr[j],arr[i]];
+    }
+    return arr;    
+
+  }
+
+  function match () {
+    if(cartasEscogidas[0] === cartasEscogidas[1]) {
+      alert('hiciste match');
+    } else {
+       alert('te equivocaste');
+       cartasEscogidas.length = 0;
+    }}  
 
 
-
-export default App;
-// import pokemon from "../data/pokemon/pokemon.js";
-
-// const App = () => {
-//   const mostrarGameBoard = document.createElement("div");
-//   mostrarGameBoard.className = "listaDePokemones";
-
-//   let lista = recortarLista(pokemon.items, 3);
-//   shuffle(lista);
-//   let cartasEscogidas = [];
-
-//   function flipCard(target) {
-//     cartasEscogidas.push(target);
-//     if(cartasEscogidas.length === 2) {
-//       setTimeout(match, 1000);
-//     }
-//     console.log(target);
-//     console.log(cartasEscogidas)
-//   }
-  
-//   function match () {
-//     if(cartasEscogidas[0] === cartasEscogidas[1]) {
-//       alert('hiciste match');
-//     } else {
-//       alert('te equivocaste');
-//       cartasEscogidas.length = 0;
-//     }
-//   }
-
-//   for (let i = 0; i < lista.length; i++) {
-  
-//     let card = document.createElement("div");
-//     card.className = "card";
-//     let imageFront = document.createElement("img");
-//     imageFront.id = i+"";
-//     imageFront.src = "ImageFront1.png";
-//     imageFront.classname = "cardFrontImage";
-//     card.appendChild(imageFront);
-
-//     let imagenBack = document.createElement('img');
-//     imagenBack.id = i;
-//     let cardId = imagenBack.id;
-//     imagenBack.className = "card-back-img";
-    
-//     imagenBack.setAttribute("src", lista[cardId].image);
-//     card.appendChild(imagenBack);
-
-//     card.addEventListener("click", (event)=> {
-//       card.classList.toggle('is-flipped');
-//       flipCard(event.target);
-//     });        
-//   mostrarGameBoard.appendChild(card)
-//   }
-//   // return mostrarGameBoard;
-// };
-
-// function recortarLista(lista) {
-//   const listaRecortada = [];
-//   for (let index = 0; index < 3; index++) {
-//     const element = lista[index];
-//     listaRecortada.push(element, element); //aquí se duplica un elemento de la lista, hasta llegar al index 3 (los tres primeros elemento
-//   }
-//   return listaRecortada;
-// }
-// const shuffle = (arr) =>{
-//     for(var i =arr.length-1 ; i>0 ;i--){
-//         var j = Math.floor( Math.random() * (i + 1) );
-//         [arr[i],arr[j]]=[arr[j],arr[i]];
-//     }
-//     //return arr;    
-
-//   }
-// /*let im =  shuffle;
-
-
-
-//   )
-// }*/
-
-// export default App;
+ export default App;
