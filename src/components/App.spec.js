94acfb54841250d6d0b08fe9528aad5c15244922
ConsueloHerrera <consueloherrerabarguena@gmail.com>
2021-06-30 11:01:49 -0400

    primer test app como function

--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -1,13 +1,12 @@
 import App from './App.js';
-import {shuffle} from './App.js';
+// import {shuffle} from './App.js';
 
 describe('App', () => {
   it('should render without crashing', () => {
     const el = App();
     expect(el instanceof HTMLElement).toBe(true);
   });
-  it('should render without crashing', () => {
-    const el = App();
-    expect(el instanceof HTMLElement).toBe(true);
+  it('should be a function', () => {
+    expect(typeof App).toBe("function");
   });
 });
