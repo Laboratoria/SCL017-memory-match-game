d3cca1c652a10b6b1ebbb99e7ff22f612367ca32
Vania Venegas <venegas.vania@gmail.com>
2021-07-01 09:38:27 -0400

    footer + cartas

--- src/data/emotions/emotions.js
+++ src/data/emotions/emotions.js
@@ -1,15 +1,15 @@
 export default {
   name: 'Emotions',
   items:[
-    { id: 'asustado', image: "https://www.lifeder.com/wp-content/uploads/2018/02/ni%C3%B1o-miedo-ara%C3%B1a.jpg", bgColor: '#339933' },
-    { id: 'enfadado', image: "https://static.guiainfantil.com/media/19735/c/hugo-aprende-a-controlar-su-ira-cuento-corto-para-ninos-de-la-rabia-xs.jpg" , bgColor: '#339933' },
-    { id: 'contenta', image: "https://image.freepik.com/vector-gratis/vuelta-escuela-nino-salto-contento-ir-escuela-sol-manana_68708-363.jpg" , bgColor: '#339933' },
-    { id: 'preocupado', image: "https://previews.123rf.com/images/anggar3ind/anggar3ind1701/anggar3ind170100362/68542302-confundido-ni%C3%B1o-en-responder-a-crucigrama.jpg", bgColor: '#F05032' },
-    { id: 'aburrida', image: "https://static.guiainfantil.com/media/16317/c/me-aburro-cuentos-cortos-para-ninos-sobre-el-aburrimiento-en-casa-lg.jpg", bgColor: '#F05032' },
-    { id: 'avergonzada', image: "https://image.shutterstock.com/image-vector/young-shy-woman-embarrassed-expression-260nw-1235744065.jpg", bgColor: '#F05032' },
-    { id: 'enamorado', image: "http://1.bp.blogspot.com/-ULzbXcm2qN4/TZNh-ad2biI/AAAAAAAAAFg/_WoGcFDuefQ/s1600/enamorada_6j3.jpg", bgColor: '#1572B6' },
-    { id: 'sorprendida', image: "https://i0.wp.com/thumbs.dreamstime.com/b/la-emoci%C3%B3n-de-sorpresa-joven-sorprendido-elemento-para-una-incre%C3%ADble-y-sorprendente-noticia-o-sugerencia-157925274.jpg", bgColor: '#1572B6' },
-    { id: 'agobiado', image:  "https://thumbs.dreamstime.com/b/el-activar-gordo-del-hombre-cansado-31019325.jpg", bgColor: '#1572B6' },
+    { id: 'aburrido', image: "https://i.postimg.cc/XNHrJ7Gz/caraaburrido.png", bgColor: '#339933' },
+    { id: 'apenado', image: "https://i.postimg.cc/658yGJC5/caraapenado.png" , bgColor: '#339933' },
+    { id: 'asustado', image: "https://i.postimg.cc/pdHT7m4p/caraasustado.png" , bgColor: '#339933' },
+    { id: 'decepcionada', image: "https://i.postimg.cc/X7tXfHgK/caradecepcionada.png", bgColor: '#F05032' },
+    { id: 'disgustada', image: "https://i.postimg.cc/7Z7hm3BF/caradisgustada.png", bgColor: '#F05032' },
+    { id: 'enojado', image: "https://i.postimg.cc/mryDGbWV/caraenojado.png", bgColor: '#F05032' },
+    { id: 'feliz', image: "https://i.postimg.cc/B6hnXM6K/carafeliz.png", bgColor: '#1572B6' },
+    { id: 'sorprendida', image: "https://i.postimg.cc/qqzMnyhZ/carasorprendida.png", bgColor: '#1572B6' },
+    { id: 'triste', image:  "https://i.postimg.cc/zBDfz9JN/caratriste.png", bgColor: '#1572B6' },
   ],
 };
  
